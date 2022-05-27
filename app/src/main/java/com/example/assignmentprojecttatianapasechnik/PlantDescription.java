1c011414a9c75e605ccfa014c8e57f80cf0396e5
Tatiana <tatjana.pasechnik@gmail.com>
2022-05-27 19:27:23 +1200

    new updates

--- app/src/main/java/com/example/assignmentprojecttatianapasechnik/PlantDescription.java
+++ app/src/main/java/com/example/assignmentprojecttatianapasechnik/PlantDescription.java
@@ -5,19 +5,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class PlantDescription extends AppCompatActivity {
+
+    TextView plantHeadign;
+    ImageView plantPhotoBackground;
+    ImageView plantPhoto;
+    TextView description1;
+    TextView description2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant_description);
+        Bundle bundle = getIntent().getExtras();
+        if(bundle!=null)
+        {
+
+        //Set data according to plant clicked
+        plantHeadign=findViewById(R.id.plant_heading);
+        plantHeadign.setText(getIntent().getStringExtra("PlantHeading"));
+
+        plantPhotoBackground = findViewById(R.id.pictureBackground);
+        plantPhotoBackground.setImageResource(bundle.getInt("BackgroundPhoto"));
+
+        plantPhoto = findViewById(R.id.picture);
+        plantPhoto.setImageResource((bundle.getInt("PlantPhoto")));
+
+        description1= findViewById(R.id.firstDescription);
+        description1.setText(bundle.getString("description1"));
+
+        description2 = findViewById(R.id.secondDescription);
+        description2.setText(bundle.getString("description2"));
 
         TextView mainPage = (TextView) findViewById(R.id.backToMain);
         mainPage.setOnClickListener(backToMain);
         TextView categoryPage = (TextView) findViewById(R.id.backToCategory);
         categoryPage.setOnClickListener(backToCategory);
+        }
 
     }
 
